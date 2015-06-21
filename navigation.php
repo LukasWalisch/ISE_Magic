301d9a8d2f88682bc9917ab524cdfa14a20f3ffe
Richard Dworschak <richard22412@hotmail.com>
2015-06-21 13:48:48 +0200

    Fuuuuuck

--- navigation.php
+++ navigation.php
@@ -5,7 +5,16 @@
 	<title>Navigation</title>
 </head>
 <body>
+Hallo1
+
 	<?php
+        if ($_POST["username"] != "admin" && $_POST["password"] != "admin")
+        {
+           include("Fail.html");
+            return;
+        }
+
+
 		$card = $_GET["Kartenname"];
 		if ($card != NULL)
 		{
