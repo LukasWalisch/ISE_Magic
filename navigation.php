3240c1b8f37e26abc649137ed69b704b61c38cee
Jenny Lien <a1302531@unet.univie.ac.at>
2015-06-22 15:15:49 +0200

    <!-- Bootstrap core JavaScript && jquery -->     <script src="js/bootstrap.min.js"></script>     <script src="js/jquery.js"></script>
        reihenfolge geaendert und hinzugefuegt

--- navigation.php
+++ navigation.php
@@ -2,10 +2,32 @@
 <html lang="de">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="SS14 IS Engineering (PR), Universitaet Wien">
+    <meta name="author" content="Team 13">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="main.css" rel="stylesheet">
 
 	<title>Navigation</title>
 </head>
 <body>
+    <div class="container">
+        <!-- SUCHFUNKTION -->
+        <div class="col-md-12">
+
+        </div>
+    </div>
+
+
+
+
+
+
+
 Hallo1
 
 	<?php
