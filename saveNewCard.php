98f29868d2318edb189a55472aa88daa2cb0e630
LukasWalisch <lukas.walisch@hotmail.com>
2015-06-24 10:49:36 +0200

    lkml,svfvsdfv

---
+++ saveNewCard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="SS15 IS Engineering (PR), Universitaet Wien">
+    <meta name="author" content="Team 13">
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/main.css" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body>
+
+
+
+</body>
+
+</html>
